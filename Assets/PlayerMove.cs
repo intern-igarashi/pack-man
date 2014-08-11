6934d34b321e5d6c59eb85cf62c660d1791adafa
intern-igarashi <tkintern04@gamil.com>
2014-08-11 16:59:37 +0900

    プレイヤーの基本動作

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour 
+{
+	private const float MOVE_SPEED = 0.1f; 
+
+	Quaternion START_ROTATION = Quaternion.Euler (0, -90, 0);
+
+	// Use this for initialization
+	void Start () 
+	{
+		transform.rotation = START_ROTATION;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.UpArrow)) 
+		{
+			transform.rotation = START_ROTATION*Quaternion.Euler (-90, 0 , 0);
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			transform.rotation = START_ROTATION*Quaternion.Euler (90, 0 , 0);
+		}
+		else if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			transform.rotation = START_ROTATION*Quaternion.Euler (180, 0 , 0);
+		}
+		else if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			transform.rotation = START_ROTATION*Quaternion.Euler (0, 0 , 0);
+		}
+
+		transform.position += transform.forward*MOVE_SPEED;
+	}
+}
+
