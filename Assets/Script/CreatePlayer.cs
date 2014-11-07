6474ee3213d034c63e3f252ea5efe120e6d5429c
igarashi nobuyuki <nobujojo.0501@hotmail.co.jp>
2014-11-07 18:32:45 +0900

    Refactoring of CreatePlayer.cs

--- Assets/Script/CreatePlayer.cs
+++ Assets/Script/CreatePlayer.cs
@@ -1,88 +1,58 @@
+/// <summary>
+/// Create Network Charactor.
+/// </summary>
+
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
 public class CreatePlayer : Photon.MonoBehaviour 
 {
-	GameObject gameManager;
+	
+	int createType;
+	const int pacman_id = 1;
+	Dictionary<int, string> createCharactorType;
+	
+	const int init_charactor_num_max = 5 + pacman_id;
 
-	int type;
-
-	enum PLAYER_TYPE
-	{
-		PACMAN = 1,
-		RED_MONSTER,
-		BLUE_MONSTER,
-		GREEN_MONSTER,
-		PINK_MONSTER
+	// file path
+	const string prefab_path = "Prefab/";
+	sring[] createCharactorPath = {
+		"pacman", "redMonster", "blueMonster", "greenMonster", "pinkMonster"
 	};
 
-	const string prefabPath = "Prefab/";
+	void InitCharactorType()
+	{
+		createCharactorType = new Dictionary<int, string> ();
+		for (int i = pacman_id; i < init_charactor_num_max; i++)
+		{
+			createCharactorType [i] = prefab_path + createCharactorPath[i];
+		}
+	}
 
-	string pacmanName = prefabPath + "pacman";
-	string redMonsterName = prefabPath + "redMonster";
-	string blueMonsterName = prefabPath + "blueMonster";
-	string greenMonsterName = prefabPath + "greenMonster";
-	string pinkMonsterName = prefabPath + "pinkMonster";
+	public void CreateNetworkCharactor(int charactorType)
+	{
+		InitCharactorType ();
+		Vector3 createPos = Vector3.zero;
 
-	Dictionary<int, string> playerType;
-	
-//	public void CreateRoomCharactor()
-//	{
-//		gameManager = GameObject.FindWithTag("GameManager");
-//		type = PhotonNetwork.playerList.Length;
-//		playerTypeInit ();
-//		
-//		// ホストならパックマンになる.
-//		if (type == (int)PLAYER_TYPE.PACMAN)
-//		{
-//			PhotonNetwork.Instantiate (playerType[(int)PLAYER_TYPE.PACMAN], new Vector3(((float)type-2.5f)*3, 0f, 0f), Quaternion.identity, 0);
-//			gameManager.GetComponent<GameManager>().SetControlPlayerType((int)PLAYER_TYPE.PACMAN);
-//		}
-//		else
-//		{
-//			PhotonNetwork.Instantiate (playerType[type], new Vector3(((float)type-2.5f) * 3, 0f, 0f), Quaternion.identity, 0);
-//			gameManager.GetComponent<GameManager>().SetControlPlayerType(type);	
-//		}
-//	}
+		if (createType == pacman_id)
+		{
+			createPos = new Vector3(0f, -2f, 0f);
+		}
+		else
+		{
+			createPos = new Vector3(charactorType-3f, 0f, 0f);
+		}
+		PhotonNetwork.Instantiate (createCharactorType[charactorType], createPos, Quaternion.identity, 0);
+	}
 
 	public GameObject CreateRoomCharactor()
 	{
-		type = PhotonNetwork.playerList.Length;
-		playerTypeInit ();
-		
-		// ホストならパックマンになる.
-		if (type == (int)PLAYER_TYPE.PACMAN)
-		{
-			return PhotonNetwork.Instantiate (playerType[(int)PLAYER_TYPE.PACMAN], new Vector3(((float)type-2.5f)*3, 0f, 0f), Quaternion.identity, 0);
-		}
-		else
-		{
-			return PhotonNetwork.Instantiate (playerType[type], new Vector3(((float)type-2.5f) * 3, 0f, 0f), Quaternion.identity, 0);
-		}
-		return null;
-	}
+		InitCharactorType ();
+		createType = PhotonNetwork.playerList.Length;
 
-	public void InitPlayerType(int charactorType)
-	{
-		playerTypeInit ();
-		if (type == (int)PLAYER_TYPE.PACMAN)
-		{
-			PhotonNetwork.Instantiate (playerType[(int)PLAYER_TYPE.PACMAN], new Vector3(0f, -2f, 0f), Quaternion.identity, 0);
-		}
-		else
-		{
-			PhotonNetwork.Instantiate (playerType[charactorType], new Vector3(charactorType-3f, 0f, 0f), Quaternion.identity, 0);
-		}
-	}
-
-	void playerTypeInit()
-	{
-		playerType = new Dictionary<int, string> ();
-		playerType [(int)PLAYER_TYPE.PACMAN] = pacmanName;
-		playerType [(int)PLAYER_TYPE.RED_MONSTER] = redMonsterName;
-		playerType [(int)PLAYER_TYPE.BLUE_MONSTER] = blueMonsterName;
-		playerType [(int)PLAYER_TYPE.GREEN_MONSTER] = greenMonsterName;
-		playerType [(int)PLAYER_TYPE.PINK_MONSTER] = pinkMonsterName;
+		Vector3 createPos = new Vector3 (((float)createType - 2.5f) * 3, 0f, 0f);
+	
+		return PhotonNetwork.Instantiate (createCharactorType[createType], createPos, Quaternion.identity, 0);
 	}
 }
